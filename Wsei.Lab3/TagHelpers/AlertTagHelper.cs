5acba89d3a85108fd90d12a5f2ee9432fd1eecd9
Patryk Buzowicz <patrykbuzowicz@gmail.com>
2021-12-05 08:00:41 +0100

    tag helper

---
+++ Wsei.Lab3/TagHelpers/AlertTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wsei.Lab3.TagHelpers
+{
+    [HtmlTargetElement("alert")]
+    public class AlertTagHelper : TagHelper
+    {
+        public string Color { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes.Add("class", "alert alert-primary");
+        }
+    }
+}
